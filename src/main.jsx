203f7cee6ec80adac62c465906ae5885911d7110
Welington Silva <welingtonsilva401@gmail.com>
2024-11-15 06:52:04 -0300

    ajuste na rota principal

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />}>
+        <Route path="/Hackaton-CPDI-ODS" element={<App />}>
           <Route index element={<Home />} />
 
           <Route path="water" element={<Water />}>
